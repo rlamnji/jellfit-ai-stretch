8cceb14a813636f0e7b98353be800c82e4b430b4
DaramLee <leeminju913@gmail.com>
2025-04-10 00:08:48 +0900

    [Feat] tailwindcss 설치
    taillwindcss, postcss, autoprefixer 설치 및 세팅 완료
    앞으로 tailwind 적용 가능함.

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // 컴포넌트가 있는 위치
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
