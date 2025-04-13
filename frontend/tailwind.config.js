9700e46af53bf470bd4c58cacf0503f3e707f261
DaramLee <leeminju913@gmail.com>
2025-04-13 17:58:45 +0900

    [Design] 로그인 화면 2차 구현
    대충 완성함. 로그인버튼 활성화됨. 로그인 데이터 처리는 포함 안됨.

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -4,7 +4,17 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}", // 컴포넌트가 있는 위치
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'input-container-color' : '#FFF5E0',
+        'input-text-color' : '#552F2F',
+        'button-color' : '#552F2F',
+        'disabled-button-color' : "#997F7F"
+      },
+      backgroundImage: {
+        'space' : "url('./assets/images/etc/space.png')"
+      }
+    }
   },
   plugins: [],
 }
