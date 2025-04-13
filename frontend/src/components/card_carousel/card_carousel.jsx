0baffcbe22dd542096eefbaa52fb0d51a1356e1c
DaramLee <leeminju913@gmail.com>
2025-04-13 20:27:43 +0900

    [Design] 가이드 모드 선택 페이지 틀 구현
    선택 페이지 틀 구현. 껍데기로 돌아감. 이것저것 미완성이어서 주석보고 더 개발해야됨.
    데이터 처리도 아직 안됨.

--- frontend/src/components/card_carousel/card_carousel.jsx
+++ frontend/src/components/card_carousel/card_carousel.jsx
@@ -4,11 +4,11 @@ import PreviewCard from "./preview_card";
 import imgLeft from '../../assets/images/left.png';
 import imgRight from "../../assets/images/right.png";
 
-function CardCarousel({bodyCards}){
+function CardCarousel({cards}){
     const [currentIndex, setcurrentIndex] = useState(1);
     const [leftIndex, setLeftIndex] = useState(currentIndex - 1);
     const [rightIndex, setRightIndex] = useState(currentIndex + 1);
-    const lastIndex = bodyCards.length - 1;
+    const lastIndex = cards.length - 1;
     const firstIndex = 0;
     const moveLeft = () =>{
         if(currentIndex === lastIndex - 1){
@@ -40,21 +40,21 @@ function CardCarousel({bodyCards}){
             setRightIndex(currentIndex);
         }
     }
-    console.log(bodyCards[0]);
+    console.log(cards[0]);
     return(
-        <div>
-            <div className="carouselItems" class="flex justify-center items-center">
-                {/* props으로 인덱스를 넘겨야할지 bodyCards값 자체를 넘겨야할지 뭐가 더 나은지 몰겄다.. */}
-                <PreviewCard card={bodyCards[leftIndex]}/> 
-                <FocusedCard card={bodyCards[currentIndex]}/>
-                <PreviewCard card={bodyCards[rightIndex]}/>
+        <div className="relative p-8">
+            <div className="carouselItems flex justify-center items-center">
+                {/* props으로 인덱스를 넘겨야할지 cards값 자체를 넘겨야할지 뭐가 더 나은지 몰겄다.. */}
+                <PreviewCard card={cards[leftIndex]}/> 
+                <FocusedCard card={cards[currentIndex]}/>
+                <PreviewCard card={cards[rightIndex]}/>
             </div>
             {/* 위치 fix하기 */}
-            <div class="fixed">
-                <button id="left_btn" onClick={moveLeft}>
+            <div className="flex justify-between w-[390px] h-16 absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
+                <button id="left_btn" className="w-16 h-16" onClick={moveLeft}>
                     <img src={imgLeft} alt="왼쪽 버튼" />
                 </button>
-                <button id="right_btn" onClick={moveRight}>
+                <button id="right_btn" className="w-16 h-16" onClick={moveRight}>
                     <img src={imgRight} alt="오른쪽 버튼" />
                 </button>
             </div>
--- frontend/src/components/card_carousel/preview_card.jsx
+++ frontend/src/components/card_carousel/preview_card.jsx
@@ -2,14 +2,14 @@ import img from "../../assets/images/etc/character.png";
 
 function PreviewCard({card}){
     return(
-        <div class="bg-white w-[280px] h-[200px] p-4 rounded-lg">
-            <div>
-                <img src={img} alt="신체부위" />
+        <div class="bg-white w-[280px] h-[200px] p-4 rounded-3xl">
+            <div className=" h-fit border-2 rounded-xl mb-2">
+                <img className='h-[80px]'src={img} alt="신체부위" />
             </div>
             <div>
-                <h1 class="font-bold">{card.name}</h1>
-                <div>스트레칭 효과</div>
-                <div>{card.effect}</div>
+                <h1 className="mb-1.5 font-semibold text-lg">{card.name}</h1>
+                <div className="mb-1 font-semibold text-sm">스트레칭 효과</div>
+                <div className="text-[#979797] text-xs">{card.effect}</div>
             </div>
         </div>
     );
