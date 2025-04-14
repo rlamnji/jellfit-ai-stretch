4bcb67883b06cd12ad95475d52ce0d883677857f
DaramLee <leeminju913@gmail.com>
2025-04-14 10:51:14 +0900

    [Refactor] 라우터 병합
    홈화면 라우터랑 다른 기타 화면의 라우터를 하나의 파일에 병합함.

--- frontend/src/routes/router.jsx
+++ frontend/src/routes/router.jsx
@@ -8,13 +8,17 @@ import ConditionPage from '../pages/condition/condition_page';
 import FriendPage from '../pages/home/dash_board/friend_page';
 import CollectPage from '../pages/home/dash_board/collect_page';
 import UserProfilePage from '../pages/home/user_profile_page';
+import GuideByBodyPage from "../pages/guide/guide_by_body";
+import GuideBySituationPage from "../pages/guide/guide_by_situation";
+import DiaryPage from "../pages/diary/diary_page";
 
 function Router() {
     return (
         <BrowserRouter>
             <Routes>
-
                 <Route path="/" element={<LoginPage />} />
+                {/* 로그인과 회원가입 */}
+                <Route path="/login" element={<LoginPage />} />
                 <Route path="/join" element={<JoinPage />} />
                 <Route path="/condition" element={<ConditionPage />} />
                 
@@ -23,6 +27,14 @@ function Router() {
                 <Route path="/home/friends" element={<FriendPage />} />
                 <Route path="/home/collection" element={<CollectPage />} />
                 <Route path="/home/userProfile" element={<UserProfilePage />} />
+
+                {/* 가이드 화면 */}
+                <Route path="/guide/body" element={<GuideByBodyPage />} />
+                <Route path="/guide/situation" element={<GuideBySituationPage />} />
+            
+                    
+                {/* 일지 화면 */}   
+                <Route path="/diary" element={<DiaryPage />} />
 
             </Routes>
         </BrowserRouter>
