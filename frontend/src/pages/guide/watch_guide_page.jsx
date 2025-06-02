804ea27d135a2d2e4f988bee5c434a7b38697fc2
DaramLee <leeminju913@gmail.com>
2025-06-03 02:49:27 +0900

    [Design] 스트레칭 영상 화면 디자인 수정

--- frontend/src/pages/guide/watch_guide_page.jsx
+++ frontend/src/pages/guide/watch_guide_page.jsx
@@ -1,7 +1,7 @@
 import TopBar from "../../components/top_bar";
 import { Link, useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
-// 동적으로 불러오는 영상이기 때문에, 영상 주소를 props로 받아서 사용해야 함. (미완성)
+
 function WatchGuidePage({ stretchingOrder }){
     const { stretchingId } = useParams();
     const [stretching, setStretching] = useState(null);
@@ -39,13 +39,13 @@ function WatchGuidePage({ stretchingOrder }){
     return(
         <div className="w-full h-screen flex flex-col items-center bg-space">
             <TopBar />
-            <div className="header w-full h-[20%] border-white">
+            <div className="header p-4 w-full h-[20%] border-white">
                 <Link to={`/guide/userStretching/${stretchingId}`} className="flex items-center justify-center w-[100px] h-[40px] bg-[#FBF2E6] font-extrabold text-[#5D3939] rounded-3xl">건너뛰기 ➜</Link>
                 <div className="ml-[45%] text-2xl font-extrabold text-[#FBF2E6]">
                     {stretching.name}
                 </div>
             </div>
-            <iframe src={stretching.videoURL} frameborder="0" className="w-[90%] h-[60%] mt-16"></iframe>
+            <iframe src={stretching.videoURL} frameborder="0" className="w-[80%] h-[60%] mt-16 rounded-xl"></iframe>
 
         </div>
     );
