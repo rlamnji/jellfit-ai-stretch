b702f2041717925f0c4c45482a180e45fdc75227
rlamnji <rlamnji@naver.com>
2025-06-03 11:28:38 +0900

    UI: 상세프로필 ui 수정

--- frontend/src/components/user_profile/user_symptom_modal.jsx
+++ frontend/src/components/user_profile/user_symptom_modal.jsx
@@ -9,32 +9,34 @@ function SymptomModal({onClose}){
         return (
             <div className='fixed top-0 left-0 w-screen h-screen bg-black bg-opacity-60 z-[99999] flex justify-center items-center pointer-events-auto'>
                 
-                <div className='relative flex flex-col overflow-hidden'> {/* 통증 전체 이미지 */}
+                <div className='relative flex flex-col overflow-hidden'> {/* 개발자 정보 */}
                     <div className=" flex flex-col absolute top-7 left-1/2 -translate-x-1/2 ">
                         <div className='h-[150px]'>
-                            <div className='text-[25px] text-[#7E6161] font-bold mb-4 ml-4 mt-2'>통증 부위</div>
+                            <div className='text-[25px] text-[#7E6161] font-bold mb-4 ml-4 mt-2'>개발자 정보</div>
                             <div className='flex text-center items-center justify-center text-[20px] bg-[#552F2F] text-white w-[130px] h-[40px] rounded-xl font-semibold mb-4 mt-8'>전체</div>
-                            <pre className="font-sans whitespace-pre-wrap w-[400px] h-[400px] overflow-y-auto text-[#85756b] text-[14px] z-[10]">* 최대 5개까지 선택 가능</pre>
+                            <pre className="font-sans whitespace-pre-wrap w-[400px] h-[400px] overflow-y-auto text-[#85756b] text-[14px] z-[10]">* 젤핏(JellFit) 개발 파트입니다.</pre>
                         </div>
-                        <div className='flex flex-col mt-8 h-[250px]'>
+                        <div className='flex flex-col mt-14 h-[250px] items-start gap-4'>
                             <div className='flex flex-row gap-2 justify-center'>
-                                <div className='flex text-center items-center justify-center text-[20px] bg-[#E2D9C9] text-[#7E6161] w-[120px] h-[50px] rounded-3xl mb-4 cursor-pointer'>거북목</div>
-                                <div className='flex text-center items-center justify-center text-[20px] bg-[#E2D9C9] text-[#7E6161] w-[120px] h-[50px] rounded-3xl mb-4 cursor-pointer'>거북목</div>
-                                <div className='flex text-center items-center justify-center text-[20px] bg-[#E2D9C9] text-[#7E6161] w-[120px] h-[50px] rounded-3xl mb-4 cursor-pointer'>거북목</div>
+                                <div className='flex text-center items-center justify-center text-[20px] bg-[#E2D9C9] text-[#7E6161] w-[120px] h-[50px] rounded-3xl mb-4 cursor-pointer'>정혜윤</div>
+                                <div className='flex text-center items-center justify-center text-[20px] text-[#7E6161] w-[120px] h-[50px] mb-4 cursor-pointer'>Team Leader</div>
+                                <div className='flex text-center items-center justify-center text-[20px] text-[#7E6161] w-[120px] h-[50px] mb-4 cursor-pointer'>AI</div>
                             </div>
-                            <div className='flex flex-row gap-5 justify-center'>
-                                <div className='flex text-center items-center justify-center text-[20px] bg-[#E2D9C9] text-[#7E6161] w-[120px] h-[50px] rounded-3xl mb-4 cursor-pointer'>거북목</div>
-                                <div className='flex text-center items-center justify-center text-[20px] bg-[#E2D9C9] text-[#7E6161] w-[120px] h-[50px] rounded-3xl mb-4 cursor-pointer'>거북목</div>
-                            </div> 
                             <div className='flex flex-row gap-2 justify-center'>
-                                <div className='flex text-center items-center justify-center text-[20px] bg-[#E2D9C9] text-[#7E6161] w-[120px] h-[50px] rounded-3xl mb-4 cursor-pointer'>거북목</div>
-                                <div className='flex text-center items-center justify-center text-[20px] bg-[#E2D9C9] text-[#7E6161] w-[120px] h-[50px] rounded-3xl mb-4 cursor-pointer'>거북목</div>
-                                <div className='flex text-center items-center justify-center text-[20px] bg-[#E2D9C9] text-[#7E6161] w-[120px] h-[50px] rounded-3xl mb-4 cursor-pointer'>거북목</div>
+                                <div className='flex text-center items-center justify-center text-[20px] bg-[#E2D9C9] text-[#7E6161] w-[120px] h-[50px] rounded-3xl mb-4 cursor-pointer'>김민주</div>
+                                <div className='flex text-center items-center justify-center text-[20px] text-[#7E6161] w-[120px] h-[50px] mb-4 cursor-pointer'>BackEnd (main)</div>
+                                <div className='flex text-center items-center justify-center text-[20px] text-[#7E6161] w-[120px] h-[50px] mb-4 cursor-pointer'>DB</div>
                             </div>
-                            <div className='flex flex-row gap-5 justify-center'>
-                                <div className='flex text-center items-center justify-center text-[20px] bg-[#E2D9C9] text-[#7E6161] w-[120px] h-[50px] rounded-3xl mb-4 cursor-pointer'>거북목</div>
-                                <div className='flex text-center items-center justify-center text-[20px] bg-[#E2D9C9] text-[#7E6161] w-[120px] h-[50px] rounded-3xl mb-4 cursor-pointer'>거북목</div>
-                            </div>                                                                                   
+                            <div className='flex flex-row gap-2 justify-center'>
+                                <div className='flex text-center items-center justify-center text-[20px] bg-[#E2D9C9] text-[#7E6161] w-[120px] h-[50px] rounded-3xl mb-4 cursor-pointer'>김민지</div>
+                                <div className='flex text-center items-center justify-center text-[20px] text-[#7E6161] w-[120px] h-[50px] mb-4 cursor-pointer'>FrontEnd</div>
+                                <div className='flex text-center items-center justify-center text-[20px] text-[#7E6161] w-[120px] h-[50px] mb-4 cursor-pointer'>BackEnd (sub)</div>
+                            </div>
+                            <div className='flex flex-row gap-2 justify-center'>
+                                <div className='flex text-center items-center justify-center text-[20px] bg-[#E2D9C9] text-[#7E6161] w-[120px] h-[50px] rounded-3xl mb-4 cursor-pointer'>이민주</div>
+                                <div className='flex text-center items-center justify-center text-[20px] text-[#7E6161] w-[120px] h-[50px] mb-4 cursor-pointer'>FrontEnd</div>
+                                <div className='flex text-center items-center justify-center text-[20px] text-[#7E6161] w-[120px] h-[50px] mb-4 cursor-pointer'>AI</div>
+                            </div>                                                                                  
                         </div>
 
                     </div>
@@ -45,11 +47,6 @@ function SymptomModal({onClose}){
                         <img src={setCancel}/>
                     </div>
 
-                    <div className='absolute bottom-7 left-1/2 -translate-x-1/2'>
-                        <div className='flex w-[150px] h-[50px] bg-[#552F2F] rounded-2xl justify-center items-center cursor-pointer'>
-                            <div className='text-[20px] text-white text-center'>저장</div>
-                        </div>
-                    </div>
                     
                 </div>
             
